9d355ab33e10d72ccc13a346d55aeabfa932ea46
Rigoberto09 <rigobertoborjas3@gmail.com>
2024-02-01 22:13:07 -0600

    Desarrollo de bases para crear el formulario.

---
+++ myApp/src/formulario/formulario.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-formulario',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.css'
+})
+export class FormularioComponent {
+
+}
+
